cb02eef6be0b0ee6e948cbb880eac7b3dd708868
ClaudioEto <115515095+ClaudioEto@users.noreply.github.com>
2023-12-04 21:57:41 -0300

    Add files via upload

---
+++ modos.js
@@ -0,0 +1,23 @@
+var btn = document.querySelector('.modos');
+var html = document.querySelector('html');
+
+btn.addEventListener('click', function () {
+    if (html.style.background === 'tomato') {
+        html.style.background = 'black';
+    } else {
+        html.style.background = 'tomato';
+    }
+});
+
+
+/* BOTAO DO MENU MOBILE */
+var btn = document.querySelector('.modos1');
+var html = document.querySelector('html');
+
+btn.addEventListener('click', function () {
+    if (html.style.background === 'tomato') {
+        html.style.background = 'black';
+    } else {
+        html.style.background = 'tomato';
+    }
+});
